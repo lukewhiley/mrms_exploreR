23a756850c8cb61904ebcd8eb3f7336e966664f7
Luke Whiley <luke.whiley@murdoch.edu.au>
2021-09-01 16:56:59 +0800

    Update 2021-LGW-mrms-report_generatoR_script_v2.r

--- scripts/2021-LGW-mrms-report_generatoR_script_v2.r
+++ scripts/2021-LGW-mrms-report_generatoR_script_v2.r
@@ -44,15 +44,16 @@ mrms_heatmap
 #+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
 print(paste0("Samples were considered outliers if their TIC was outside ", temp_answer_tic_nc, " % of the median "))
 print(paste0(nrow(tic_qc_fail_nc), " samples FAILED the TIC QC check ", nrow(tic_qc_fail_ltr_nc)," of which were ", qc_type ,  ". These were removed from the dataset."))
-print(paste0("The dataset now contains ", nrow(mrms_exploreR_data$data_tic_filtered), " samples"))
+print(paste0("The dataset now contains ", 
+             mrms_exploreR_data$data_tic_filtered %>% filter(type == "Sample") %>% nrow(), 
+             " samples and ",
+             mrms_exploreR_data$data_tic_filtered %>% filter(type == paste0(qc_type)) %>% nrow(),
+             " ",
+             paste0(qc_type),
+             " samples"
+))
+                                                             
 tic_check_p_nc 
-#'
-#'  
-#+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4 
-#knitr::kable(tic_qc_fail)
-#'
-#'
-#'
 #'
 #' ### 4. Individual feature intensity threshold QC check
 #' Individual feature QC check 1. Each m/z feature was assessed and were subsequently filtered out using a user defined signal intensity threshold and appearance frequency.
@@ -74,7 +75,7 @@ if(intensity_threshold_ltr == "all"){
   print(paste("An MRMS feature was kept in the dataset if it was present in ", intensity_threshold_percentage, " % of both study samples and QC samples with a signal intensity (peak height) greater than ", intensity_threshold, " counts", sep = ""))
 }
 
-paste0("QC check 1: ", nrow(percentage_of_zero_plot_data_nc %>% filter(pass == "pass")), " passed the QC check and had < 50% zero values.  ", nrow(percentage_of_zero_plot_data_nc %>% filter(pass == "fail")), " failed the QC check and had > 50% zero values")
+paste0("QC check 1: ", nrow(percentage_of_zero_plot_data_nc %>% filter(pass == "pass")), " passed the QC check.  ", nrow(percentage_of_zero_plot_data_nc %>% filter(pass == "fail")), " failed the QC check.")
 print(paste0("The dataset now contains ", nrow(percentage_of_zero_plot_data_nc %>% filter(pass == "pass")), " features"))
 #'
 #'
@@ -161,11 +162,12 @@ if(signal_drift_method == "RF"){
 #' The below plot represents the total ion count of each sample following the signal correction.
 #+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
 print(paste0("Samples were considered outliers if their TIC was outside ", temp_answer_tic_c, " % of the median "))
-print(paste0(nrow(tic_qc_fail_c), " samples FAILED the TIC QC check  ", nrow(tic_qc_fail_ltr_c)," of which were ", qc_type ,  ". These were removed from the dataset."))
+print(paste0(nrow(tic_qc_fail_c), " samples FAILED the TIC QC check, ", nrow(tic_qc_fail_ltr_c)," of which were ", qc_type ,  ". These were removed from the dataset."))
 print(paste0("The dataset now contains ", nrow(mrms_exploreR_data$corrected_data_tic_filtered), " samples"))
 tic_check_p_nc 
 #' 
 #' Following signal correction a % RSD filter in the replicate QC samples was applied to assess the performance of each feature across the total run
+#+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
 print(paste(nrow(rsd_plot_data)-length(which(rsd_plot_data$rsd_loop_rsd < 30)), " MRMS features had a LTR RSD of > 30%", sep=""))
 print(paste("Total number of MRMS features with an LTR RSD of <30% =", length(which(rsd_plot_data$rsd_loop_rsd < 30))))
 print(paste("Total number of MRMS feature with an LTR RSD of <20% =", length(which(rsd_plot_data$rsd_loop_rsd < 20))))
@@ -174,7 +176,7 @@ print(paste("Total number of MRMS feature with an LTR RSD of <10% =", length(whi
 #'
 #'
 #'
-#' ### Corrected QC visualization
+#' ### %RSD QC visualization on signal corrected data
 #'
 #'The below plot displays the % RSD post-signal correction of each of the remaining features in the replicate QC samples. Feature index is ordered by RSD%.
 #'
@@ -182,7 +184,7 @@ print(paste("Total number of MRMS feature with an LTR RSD of <10% =", length(whi
 QC_p_2_c
 #'
 #'
-#' ### 8. PCA plot to visualize final corrected dataset variance (replicate QC and samples)
+#' ### 9. PCA plot to visualize final corrected dataset variance (replicate QC and samples)
 #' PCA created with Pareto scaling
 #' 
 #' 
@@ -196,15 +198,14 @@ print(paste0("PCA created using ",
              " and ", 
              mrms_exploreR_data$data_tic_filtered_qc_filtered %>% select(contains("x")) %>% ncol(), 
              " MRMS features" ))
-print(paste0("coloured by sample/QC"))
 
 #' PCA coloured by Sample type
-#' #+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
+#+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
 QC_PCA_1_c
 #'
 #'
 #'
 #' PCA coloured by plateID
-#' #+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
+#+ echo=FALSE, message=FALSE, fig.width=10, fig.height=4
 QC_PCA_2_c
 #'
